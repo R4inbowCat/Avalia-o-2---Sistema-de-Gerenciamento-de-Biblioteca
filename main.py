acc58ae4e768b89382154d06fa36966547992af1
R4inbowCat <analuizalsantana@gmail.com>
2026-08-08 00:03:26 -0300

    cria a lista de livros cadastrados e implementa a função de cadastro com parâmetros

--- main.py
+++ main.py
@@ -2,8 +2,17 @@ import definicoes as d
 d.limpa()
 d.cabecalho()
 
-def cadastrar():
-    pass
+lista_de_livros = [] # Essa lista guardará todos os livros cadastrados no sistema.
+
+def cadastrar(titulo, autor, publicacao, codigo_isbn):
+    novo_livro = {  # Cria o dicionário do livro cadastrado com as informações fornecidas.
+        "Título": titulo,
+        "Autor": autor,
+        "Publicação": publicacao,
+        "Código ISBN": codigo_isbn,
+        "Status": "Disponível" } # Todo livro que acaba de ser cadastrado começa disponível.
+    return novo_livro
+
 def emprestimo():
     pass
 def devolucao():
@@ -28,7 +37,13 @@ while True:
     print("● 7. Sair do programa")
     opcao = int(input("\nEscolha uma opção: "))
     if opcao == 1:
-        cadastrar()
+        titulo = input("\nDigite o título do livro: ")
+        autor = input("Digite o autor: ")
+        publicacao = input("Digite o ano de publicação: ")
+        codigo_isbn = input("Digite o Código ISBN: ")
+        novo_livro = cadastrar(titulo, autor, publicacao, codigo_isbn)
+        lista_de_livros.append(novo_livro) # Adiciona o dicionário do livro na lista de livros cadastrados.
+        print("\nLivro cadastrado com sucesso!")
     elif opcao == 2:
         emprestimo()
     elif opcao == 3:
