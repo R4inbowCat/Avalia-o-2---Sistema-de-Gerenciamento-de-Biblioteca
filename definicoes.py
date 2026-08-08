b5c3da972d56ce8e0b48b0775fb2d4babe77941c
R4inbowCat <analuizalsantana@gmail.com>
2026-08-07 22:43:01 -0300

    cria o menu principal e adiciona as funções a serem utilizadas

---
+++ definicoes.py
@@ -0,0 +1,7 @@
+import os
+def limpa():
+    os.system('cls')
+
+def cabecalho():
+    print("--- Cabeçalho do programa ---")
+    print("\nAvaliação 2: Lógica de Programação \nPrograma criado por Ana Luiza Lopes - 2º A\n")
---
+++ main.py
@@ -0,0 +1,46 @@
+import definicoes as d
+d.limpa()
+d.cabecalho()
+
+def cadastrar():
+
+def emprestimo():
+
+def devolucao():
+
+def listar():
+
+def ordenar():
+
+def buscar():
+
+
+while True:
+    print("========================================")
+    print(" Sistema de Gerenciamento de Biblioteca")
+    print("========================================")
+    print("● 1. Cadastrar livro")
+    print("● 2. Registrar empréstimo")
+    print("● 3. Registrar devolução")
+    print("● 4. Listar livros")
+    print("● 5. Ordenar a listagem")
+    print("● 6. Buscar livro")
+    print("● 7. Sair do programa")
+    opcao = int(input("\nEscolha uma opção: "))
+    if opcao == 1:
+        cadastrar()
+    elif opcao == 2:
+        emprestimo()
+    elif opcao == 3:
+        devolucao()
+    elif opcao == 4:
+        listar()
+    elif opcao == 5:
+        ordenar()
+    elif opcao == 6:
+        buscar()
+    elif opcao == 7:
+        print("\nSaindo do programa...")
+        break
+    else:
+        print("\nOpção inválida, tente novamente.")
