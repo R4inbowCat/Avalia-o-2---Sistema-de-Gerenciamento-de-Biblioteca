e25503f9e7e6337e6e5e1704ed89a484c349f411
R4inbowCat <analuizalsantana@gmail.com>
2026-08-09 22:11:46 -0300

    Melhora as validações do cadastro e corrige o menu principal
    - Cria função 'input_seguro' usando try/except para permitir o cancelamento do cadastro a qualquer momento.
    
    - Muda a leitura e as opções do menu principal para texto, para evitar erros de digitação.
    
    - Atualiza regra do ano de publicação, restringindo o input entre os anos 1000 e 2026.
    
    - Implementa uma análise na lista de livros cadastrados para evitar o cadastro de códigos ISBN repetidos.

--- main.py
+++ main.py
@@ -5,12 +5,9 @@ d.cabecalho()
 lista_de_livros = [] # Essa lista guardará todos os livros cadastrados no sistema.
 
 def cadastrar(titulo, autor, publicacao, codigo_isbn):
-    novo_livro = {  # Cria o dicionário do livro cadastrado com as informações fornecidas.
-        "titulo": titulo.title(), # O .title() deixa as primeiras letras maiúsculas, para padronizar a escrita e deixar mais organizado.
-        "autor": autor.title(),
-        "publicacao": publicacao,
-        "isbn": codigo_isbn,
-        "status": "disponível" } # Todo livro que acaba de ser cadastrado começa disponível.
+    # Cria o dicionário do livro cadastrado com as informações fornecidas.
+    novo_livro = {"titulo": titulo.title(), "autor": autor.title(), "publicacao": publicacao, "isbn": codigo_isbn, "status": "disponível"}
+    # O .title() deixa as primeiras letras maiúsculas, para padronizar a escrita e deixar mais organizado.
     return novo_livro
 
 def emprestimo():
@@ -23,11 +20,19 @@ def ordenar():
     pass
 def buscar():
     pass
+def excluir_cadastro():
+    pass
+
+def input_seguro(mensagem):
+    resposta = input(mensagem).strip() # Retira os espaços vazios do que o usuário digitou, com o .strip().
+    if resposta == "0": # Verifica se o usuário digitou 0 para voltar ao menu principal.
+        raise Exception("CANCELADO") # Esse "cancelado" é apenas para indicar que o usuário cancelou a operação e não que um erro aconteceu.
+    return resposta
 
 while True: # Mantém o menu principal rodando continuamente até o usuário escolher a opção 7 para sair.
-    print("========================================")
+    print("=" * 40)
     print(" Sistema de Gerenciamento de Biblioteca")
-    print("========================================")
+    print("=" * 40)
     print("● 1. Cadastrar livro")
     print("● 2. Registrar empréstimo")
     print("● 3. Registrar devolução")
@@ -35,39 +40,90 @@ while True: # Mantém o menu principal rodando continuamente até o usuário esc
     print("● 5. Ordenar a listagem")
     print("● 6. Buscar livro")
     print("● 7. Sair do programa")
-    opcao = int(input("\nEscolha uma opção: "))
-    if opcao == 1:
-        titulo = input("\nDigite o título do livro: ")
-        autor = input("Digite o autor: ")
-        while True:
-            publicacao = input("Digite o ano de publicação: ")
-            if publicacao.isdigit() and len(publicacao) == 4: # Verifica se o ano digitado é válido (apenas números e 4 dígitos).
-                break # Se a data for válida, sai da repetição, senão continua até que seja válida.
-            else:
-                print("\nO ano de publicação digitado é inválido. Tente novamente.")
-        while True:
-            codigo_isbn = input("Digite o Código ISBN com os traços (ex: 012-34-567-8910-1): ")
-            codigo_isbn = codigo_isbn.replace(" ", "")
-            codigo_teste = codigo_isbn.replace("-", "") # Cria uma cópia temporária e usa o .replace() para remover os traços e espaços.
-            if len(codigo_teste) == 13 and codigo_teste.isdigit() and codigo_isbn.count("-") == 4: # Verifica se o código digitado tem 13 números e 4 hífens.
-                break # O loop continuará até que o código digitado seja válido.
-            else:
-                print("\nO código ISBN digitado é inválido. Tente novamente.")
-        novo_livro = cadastrar(titulo, autor, publicacao, codigo_isbn) # Chama a função cadastrar() para criar o dicionário do livro com as informações fornecidas no input.
-        lista_de_livros.append(novo_livro) # Adiciona o dicionário do livro na lista de livros cadastrados.
-        print("\nLivro cadastrado com sucesso!")
-    elif opcao == 2:
+    opcao = input("\nEscolha uma opção: ")
+
+    if opcao == "1": # Se o usuário digitar 1, entrará no bloco de cadastro de livro, mas ele pode sair quando quiser digitando 0.
+        d.limpa()
+        print("\n==== Cadastro de livro ====")
+        print("\n(Digite '0' em qualquer pergunta para cancelar e voltar ao menu principal.)")
+
+        try: # O programa vai tentar rodar este bloco inteiro.
+
+            # Parte do título:
+            while True:
+                titulo = input_seguro("\nDigite o título do livro: ")
+                if titulo == "":
+                    print("\nO título não pode ficar em branco. Tente novamente.")
+                else:
+                    break # Para todos os "while True" os loops continuarão até que o usuário digite um valor válido para cada.
+
+            # Parte do autor:
+            while True:
+                autor = input_seguro("\nDigite o autor: ")
+                if autor == "": # Verifica se o usuário não digitou nada no input, se for verdade, pede para digitar novamente.
+                    print("\nO autor não pode ficar em branco. Tente novamente.")
+                else:
+                    break
+
+            # Parte do ano de publicação:
+            while True:
+                publicacao = input_seguro("\nDigite o ano de publicação: ")
+                if publicacao == "":
+                    print("\nO ano não pode ficar em branco. Tente novamente.\n") 
+                elif publicacao.isdigit() and 1000 <= int(publicacao) <= 2026: # Verifica se a data digitada possui apenas números e está entre 1000 e 2026.
+                    break
+                else:
+                    print("\nO ano digitado é inválido (muito grande ou muito pequeno). Tente novamente.")
+
+            # Parte do código ISBN:
+            while True:
+                codigo_isbn = input_seguro("\nDigite o Código ISBN do livro: ")
+                if codigo_isbn == "":
+                    print("\nO Código ISBN não pode ficar em branco. Tente novamente.")
+                    continue
+
+                # Usa o .replace() para remover os traços e espaços, deixando só os números.
+                codigo_isbn_limpo = codigo_isbn.replace(" ", "").replace("-", "")
+
+                # 1º Teste: Verifica se o código digitado tem exatamente 13 números.
+                if not (len(codigo_isbn_limpo) == 13 and codigo_isbn_limpo.isdigit()):
+                    print("\nO código ISBN digitado é inválido (deve conter 13 números). Tente novamente.")
+                    continue
+
+                # 2º Teste: Verifica se o código ISBN já existe na lista de livros cadastrados.
+                ja_cadastrado = False
+                for livro in lista_de_livros:
+                    if livro["isbn"] == codigo_isbn_limpo:
+                        ja_cadastrado = True
+                        break # Se já tiver um código igual, o programa pede para digitar outro código ISBN.
+                if ja_cadastrado == True:
+                    print("\nEste código ISBN já está cadastrado em outro livro na biblioteca. Tente novamente.")
+                else:
+                    codigo_isbn = codigo_isbn_limpo # Se não existir outro código igual e ele passar nos testes, ele é salvo.
+                    break
+
+            # Se o usuário não digitar 0 para voltar ao menu principal, o cadastro é finalizado.
+            novo_livro = cadastrar(titulo, autor, publicacao, codigo_isbn) # Chama a função cadastrar() para criar o dicionário do livro com as informações fornecidas no input.
+            lista_de_livros.append(novo_livro) # Adiciona o dicionário do livro na lista de livros cadastrados.
+            print("\nLivro cadastrado com sucesso!")
+
+        # Se ele digitar 0, o programa vai pular para esse bloco de exceção e mostrar a mensagem de cancelamento.
+        except Exception: 
+            d.limpa()
+            print("\nCadastro cancelado. Voltando ao menu principal...")
+
+    elif opcao == "2":
         emprestimo()
-    elif opcao == 3:
+    elif opcao == "3":
         devolucao()
-    elif opcao == 4:
+    elif opcao == "4":
         listar()
-    elif opcao == 5:
+    elif opcao == "5":
         ordenar()
-    elif opcao == 6:
+    elif opcao == "6":
         buscar()
-    elif opcao == 7:
-        print("\nSaindo do programa...")
+    elif opcao == "7":
+        print("\nEncerrando o programa...")
         break
     else:
         print("\nOpção inválida, tente novamente.")
